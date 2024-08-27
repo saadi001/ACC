0a22ec7631764b4e00120d054b2ad3a459301704
Sheikh saadi <sheikhsadi1620@gmail.com>
2024-08-28 02:50:08 +0600

    contact page updatedd

--- app/components/gallery/Gallery.js
+++ app/components/gallery/Gallery.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import img from "../../asset/images/antu profile.jpg";
 import Title from "../shared/Title/Title";
+import { Dog, SaladIcon } from "lucide-react";
 
 const Gallery = () => {
   return (
@@ -11,12 +12,15 @@ const Gallery = () => {
         <Marquee pauseOnHover>
           {[1, 2, 3, 4, 5].map((details, i) => {
             return (
-              <div key={i} className="w-56 h-72 mx-2">
+              <div key={i} className="w-56 h-72 mx-2 relative">
                 <Image
                   src={img}
                   alt="gallery photo"
                   className="w-full h-full object-cover"
                 />
+                <div className="absolute inset-x-0">
+                  <p>hello</p>
+                </div>
               </div>
             );
           })}
